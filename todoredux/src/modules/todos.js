f4336ef8b5bf0c0d7c7c2ef2efae445b1954e92f
EUNHEE JANG <6810779s@naver.com>
2021-09-29 21:42:57 +0900

    refactor

--- todoredux/src/modules/todos.js
+++ todoredux/src/modules/todos.js
@@ -1,4 +1,5 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -30,10 +31,10 @@ export const toggle = createAction(TOGGLE, (id) => id);
 // });
 
 export const remove = createAction(REMOVE, (id) => id);
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
+// export const remove = (id) => ({
+//   type: REMOVE,
+//   id,
+// });
 
 const initialState = {
   input: '',
@@ -53,24 +54,38 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({
-      ...state,
-      input: action.payload,
-    }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(action.payload),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.id === action.payload ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter((todo) => todo.id !== action.payload),
-    }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    // ...state,
+    // input: action.payload,
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    // ...state,
+    // todos: state.todos.concat(action.payload),
+
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    // ...state,
+    // todos: state.todos.map((todo) =>
+    //   todo.id === action.payload ? { ...todo, done: !todo.done } : todo,
+    // ),
+
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
+    // ({
+    //   ...state,
+    //   todos: state.todos.filter((todo) => todo.id !== action.payload),
+    // }),
   },
   initialState,
 );
